3bed53ffd5ee21ca6ed8f71ad1a02ec7c2f1af86
Zach Johnson <zachjohnson@Zachs-MBP-2.attlocal.net>
2024-06-01 20:56:59 -0500

    game company site improvements

--- src/Sites/GameCompany/Components/Footer/Footer.jsx
+++ src/Sites/GameCompany/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 const Footer = () => {
   return (
     <div className='game__footer container'>
-      <h2>Copyright  {new Date().getFullYear()} All Rights Reserved.</h2>
+      <h2 className='copyright'>Copyright  {new Date().getFullYear()} All Rights Reserved.</h2>
       <ul>
         <li><Link></Link>About</li>
         <li><Link></Link>Contact</li>
--- src/Sites/GameCompany/Components/GameBg/GameBg.jsx
+++ src/Sites/GameCompany/Components/GameBg/GameBg.jsx
@@ -9,14 +9,20 @@ const GameBg = (props) => {
     
   return (
     <div className='gamebg' style = {{backgroundImage: `url(${props.bgimage})`}}>
+      <div className='bg-left'>
         <h1 className='gamebg__title'>{props.title}</h1>
-        <p className='gamebg__subtitle'>{props.subtitle}</p>
-        <img className='gamebg__gameimg' src={props.gameimage} />
-        <div className='gamebg__store-buttons'>
-            <StoreButton link={props.applelink} logo={apple_logo} small="Download on the" big="App Store"/> 
-            <StoreButton link={props.googlelink} logo={google_logo} small="GET IT ON" big="Google Play"/>
-        </div>
+          <p className='gamebg__subtitle'>{props.subtitle}</p>
+          <img className='gamebg__gameimg' src={props.gameimage} />
+          <div className='gamebg__store-buttons'>
+              <StoreButton link={props.applelink} logo={apple_logo} small="Download on the" big="App Store"/> 
+              <StoreButton link={props.googlelink} logo={google_logo} small="GET IT ON" big="Google Play"/>
+          </div>
+      </div>
+
+      <div className='bg-right'>
         <QrCode code={props.qrcode}/>
+      </div>
+
     </div>
   )
 }
