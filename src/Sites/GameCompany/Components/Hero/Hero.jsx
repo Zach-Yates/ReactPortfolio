0a13b6d927e744d4faff83e426063dd76e537caf
Zach Johnson <zachjohnson@Zachs-MBP-2.attlocal.net>
2024-06-03 16:45:48 -0500

    finalizing game company

--- src/Sites/GameCompany/Components/Hero/Hero.jsx
+++ src/Sites/GameCompany/Components/Hero/Hero.jsx
@@ -1,78 +1,179 @@
-import React, {useRef} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import './Hero.css'
 import drpop_logo from '../../../../assets/drpop-logo.png'
+import dr_logo from '../../../../assets/dr-logo.png'
+import pj_logo from '../../../../assets/pj-logo.png'
+import combattle_logo from '../../../../assets/combattle-logo.png'
 import next_icon from '../../../../assets/next-icon.png'
 import back_icon from '../../../../assets/back-icon.png'
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
-    const slider = useRef();
-    let tx = 0;
+    const [sliderIndex,setSliderIndex] = useState(0);
+    let iarr = [];
 
+    useEffect(()=>{
+        document.documentElement.style.setProperty('--slider-index',sliderIndex);
+        check();
 
+    },[sliderIndex])
+
+    const slideForward = ()=>{
+        if(sliderIndex != 3){
+            setSliderIndex(cur =>{
+                return cur + 1;
+            });
+        }else{
+            setSliderIndex(cur =>{
+                return cur =0;
+            });
+        }
+        // console.log("Next");
+    };
+
+    const slideBackward = ()=>{
+        if(sliderIndex != 0){
+            setSliderIndex(cur =>{
+                return cur - 1;
+            });
+        }else{
+            setSliderIndex(cur =>{
+                return cur = 3;
+            });
+        }
+        // console.log("Back");
+    };
+
+    const check = ()=>{
+        iarr = [...document.querySelector('.indicators').children];
+
+        iarr.forEach(e=>{
+            e.classList.remove("active");
+        })
+
+        document.querySelector('.indicators').children[sliderIndex].classList.add("active")
+    }
 
   return (
     <div className='game__hero'>
-{/* onClick={slideForward} */}
-{/* onClick={slideBackward} */}
-        <img src={next_icon} alt="" className='next-btn' />
-        <img src={back_icon} alt="" className='back-btn' />
+
+
+        <img src={next_icon} alt="" className='next-btn' onClick={slideForward}/>
+        <img src={back_icon} alt="" className='back-btn' onClick={slideBackward}/>
 
         <div className='cards'>
 
-            <div className='cards-slide'>
+        <div className='cards-slide'>
 
-                <div className='card card1'>
-                    <div className='info'>
-                        <img className='card1__logo' src={drpop_logo}/>
+            <div className='card card1'>
 
-                        <h2 className='card1__title'>
-                            Fantasy Auto Shooter
-                        </h2>
+                <div className='info'>
 
-                        <p className='card1__desc'>
-                            Weave Through Fire and Fell Foe!
-                        </p>
+                    <img className='card__logo' src={drpop_logo}/>
 
-                        <button className='card1__button'>
-                            Learn More
-                        </button>
-                    </div>
+                    <h2 className='card__title'>
+                        Auto Shooter
+                    </h2>
 
-                </div>
+                    <p className='card__desc'>
+                        Weave Through Fire and Fell Foe!
+                    </p>
 
-
-                <div className='card card1'>
-                    <div className='info'>
-                        <img className='card1__logo' src={drpop_logo}/>
-
-                        <h2 className='card1__title'>
-                            Fantasy Auto Shooter
-                        </h2>
-
-                        <p className='card1__desc'>
-                            Weave Through Fire and Fell Foe!
-                        </p>
-
-                        <button className='card1__button'>
-                            Learn More
-                        </button>
-                    </div>
+                    <Link to="/Game_Drpop">
+                    <button className='card__button'>
+                        Learn More
+                    </button>
+                    </Link>
+                  
 
                 </div>
 
             </div>
+
+            <div className='card card2'>
+
+                <div className='info'>
+
+                    <img className='card__logo' src={dr_logo}/>
+
+                    <h2 className='card__title'>
+                        Hack N Slash!
+                    </h2>
+
+                    <p className='card__desc'>
+                        Attack Block Dash and SUper
+                    </p>
+
+                    <Link to="/Game_Dr">
+                        <button className='card__button'>
+                            Learn More
+                        </button>
+                    </Link>
+                </div>
+
+            </div>
+
+            <div className='card card3'>
+
+                <div className='info'>
+
+                    <img className='card__logo' src={pj_logo}/>
+
+                    <h2 className='card__title'>
+                        Happy Picture Search!
+                    </h2>
+
+                    <p className='card__desc'>
+                        Find the 3!
+                    </p>
+
+                    <Link to="/Game_Pj">
+                        <button className='card__button'>
+                            Learn More
+                        </button>
+                    </Link>
+                </div>
+
+            </div>
+
+            <div className='card card4'>
+
+                <div className='info'>
+
+                    <img className='card__logo' src={combattle_logo}/>
+
+                    <h2 className='card__title'>
+                        Combo Action!
+                    </h2>
+
+                    <p className='card__desc'>
+                        Increase Your Combo.
+                    </p>
+
+                    <Link to="/Game_Combattle">
+                        <button className='card__button'>
+                            Learn More
+                        </button>
+                    </Link>
+
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+        </div>
+
+        <div className='indicators'>
+            <div className='indicator'></div>
+            <div className='indicator'></div>
+            <div className='indicator'></div>
+            <div className='indicator'></div>
         </div>
 
 
-
-        {/* <div className='slider'>
-            <ul ref={slider}>
-                <li>
-
-                </li>
-
-            </ul>
-        </div> */}
 
     </div>
   )
