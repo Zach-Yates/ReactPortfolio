846931cad55d936c08160612de6c3338c2d9a491
Zach Johnson <zachjohnson@Zachs-MBP-2.attlocal.net>
2024-06-11 10:02:11 -0500

    deploy fix 4

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter basename={"/ReactPortfolio/"}>
+  // <BrowserRouter basename={"/ReactPortfolio/"}>
   <App />
- </BrowserRouter>
+//  </BrowserRouter>
   // </React.StrictMode>,
 )
